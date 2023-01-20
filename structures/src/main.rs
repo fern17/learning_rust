8c15c36a4257d04ff0fc3360a98a52ce44b42b59
Fernando Nellmeldin <Fernando.Nellmeldin@euresys.com>
2023-01-20 14:27:32 +0100

    add impl for structs

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -6,6 +6,21 @@ struct Color {
 
 struct ColorTuple(u8, u8, u8);
 
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn print_description(&self) {
+        println!("Rectangle: {} x {}", self.width, self.height);
+    }
+
+    fn is_square(&self) -> bool{
+        self.width == self.height
+    }
+}
+
 fn main() {
     let bg = Color {red: 255, green: 70, blue: 50};
     println!("BG: R: {} G: {} B: {}", bg.red, bg.green, bg.blue);
@@ -17,6 +32,10 @@ fn main() {
 
     let red = ColorTuple(255, 0, 0);
     println!("Red is {} {} {}", red.0, red.1, red.2);
+
+    let rectangle = Rectangle{width: 10, height: 30};
+    rectangle.print_description();
+    println!("Rectangle is square ? {}", rectangle.is_square());
 }
 
 fn print_color(c: &Color) {
