68e90bd35d63fc0302d5b2a66675d71b613546fe
Fernando Nellmeldin <Fernando.Nellmeldin@euresys.com>
2023-01-20 16:39:08 +0100

    added arguments treating in main function

--- file_operations/src/main.rs
+++ file_operations/src/main.rs
@@ -1,8 +1,19 @@
 use std::fs::File; // struct for the file
 use std::io::prelude::*;
+use std::env;
 
 fn main() {
-    let mut file = File::open("src/file.txt").expect("Can't open file");
+    let args: Vec<String> = env::args().collect();
+    println!("Running with arguments:");
+    for argument in args.iter() {
+        println!("- {argument}");
+    }
+    let mut filename = String::from("src/file.txt");
+    if args.len() > 1 {
+        filename = args[1].clone(); // cannot borrow, so we clone
+    }
+
+    let mut file = File::open(filename).expect("Can't open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("Failed to read contents");
