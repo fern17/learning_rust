a25c0bb4e53693669f4331274446d88c231c8697
Fernando Nellmeldin <Fernando.Nellmeldin@euresys.com>
2023-01-20 13:37:48 +0100

    add control structures sample

---
+++ control_structures/src/main.rs
@@ -0,0 +1,63 @@
+enum Direction {
+    Up, 
+    Down,
+    Left,
+    Right
+}
+
+const LIMIT_NUMBER : i32 = 30;
+
+fn main() {
+    println!("If condition:");
+    let number : i32 = 35;
+    if number < LIMIT_NUMBER {
+        println!("The number {} is less than {}", number, LIMIT_NUMBER);
+    } else {
+        println!("The number {} is greater than {}", number, LIMIT_NUMBER);
+    }
+
+    println!("Infinite loop");
+    let mut n = 0;
+    loop {
+        n += 1;
+        
+        if n == 7 {
+            continue;
+        }
+
+        if n > 10 {
+            break;
+        }
+        println!("n = {}", n);
+    }
+
+    println!("While loop");
+    let mut v = 1;
+    while v <= 50 {
+        if v % 5 == 0 {
+            println!("v = {}", v);
+        }
+        v += 1;
+    }
+
+    println!("For loop");
+    let numbers = LIMIT_NUMBER..51; // range => iterator
+    for i in numbers {
+        println!("i = {}", i);
+    }
+    // We can loop in vectors, but we need to use iter to obtain an iterator
+    // With enumerate we obtain the index, and it returns a tuple
+    let animals = vec!["Dog", "Cat", "Bird"];
+    for (index, a) in animals.iter().enumerate() {
+        println!("{}: {}", index, a);
+    }
+
+    println!("Enums and match (switch)");
+    let player_direction : Direction = Direction::Up;
+    match player_direction {
+        Direction::Up => println!("We are heading up"),
+        Direction::Down => println!("We are heading down"),
+        Direction::Left => println!("We are heading left"),
+        Direction::Right => println!("We are heading right"),
+    }
+}
