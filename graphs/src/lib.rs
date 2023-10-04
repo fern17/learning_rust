2bfccbdf8d1a9abf21b409437f2b900bebdaf7b5
Fernando Nellmeldin <Fernando.Nellmeldin@euresys.com>
2023-10-04 08:32:10 +0200

    add another implementation of graph and an implementation of depth first search

--- graphs/src/lib.rs
+++ graphs/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Debug)]
 pub struct NodeNotInGraph; // error type if node does not exist
@@ -116,6 +116,100 @@ mod test_directed_graph {
         graph.add_edge(("b", "a", 5));
 
         assert_eq!(graph.neighbors("a").unwrap(), &vec![(String::from("b"), 5)]);
-        assert_eq!(graph.neighbors("b").unwrap(), &vec![(String::from("c"), 10), (String::from("a"), 5)]);
+        assert_eq!(
+            graph.neighbors("b").unwrap(),
+            &vec![(String::from("c"), 10), (String::from("a"), 5)]
+        );
+    }
+}
+
+// -------------------------------------------------------------------------------------------
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Vertex(u8);
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Edge(u8, u8);
+
+#[derive(Clone)]
+pub struct Graph {
+    #[allow(dead_code)]
+    vertices: Vec<Vertex>,
+    edges: Vec<Edge>,
+}
+
+impl Graph {
+    pub fn new(vertices: Vec<Vertex>, edges: Vec<Edge>) -> Self {
+        Graph { vertices, edges }
+    }
+}
+
+impl From<u8> for Vertex {
+    fn from(item: u8) -> Self {
+        Vertex(item)
+    }
+}
+
+impl Vertex {
+    pub fn value(&self) -> u8 {
+        self.0
+    }
+
+    pub fn neighbors(&self, graph: &Graph) -> VecDeque<Vertex> {
+        graph
+            .edges
+            .iter()
+            .filter(|e| e.0 == self.0)
+            .map(|e| e.1.into())
+            .collect()
+    }
+}
+
+impl From<(u8, u8)> for Edge {
+    fn from(item: (u8, u8)) -> Self {
+        Edge(item.0, item.1)
+    }
+}
+
+pub fn depth_first_search(graph: &Graph, root: Vertex, objective: Vertex) -> Option<Vec<u8>> {
+    let mut visited: HashSet<Vertex> = HashSet::new();
+    let mut history: Vec<u8> = Vec::new();
+    let mut queue = VecDeque::new();
+
+    queue.push_back(root);
+
+    while let Some(current_vertex) = queue.pop_front() {
+        history.push(current_vertex.value());
+        if current_vertex == objective {
+            return Some(history);
+        }
+        for neighbor in current_vertex.neighbors(graph).into_iter().rev() {
+            if visited.insert(neighbor) {
+                queue.push_front(neighbor)
+            }
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod test_graph {
+    use super::*;
+    #[test]
+    fn test_dfs() {
+        let vertices = vec![1, 2, 3, 4, 5, 6, 7];
+        let edges = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7)];
+        let root = 1;
+        let objective = 7;
+        let correct = vec![1, 2, 4, 5, 3, 6, 7];
+        let graph = Graph::new(
+            vertices.into_iter().map(|v| v.into()).collect(),
+            edges.into_iter().map(|e| e.into()).collect(),
+        );
+        assert_eq!(
+            depth_first_search(&graph, root.into(), objective.into()),
+            Some(correct)
+        );
     }
 }
